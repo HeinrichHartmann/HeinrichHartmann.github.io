ae100d74c078a02b872695e214f96eb8da911385
Heinrich Hartmann <hartmann@uni-koblenz.de>
2014-11-22 19:58:33 +0100

    Added publish script

---
+++ publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e # fail on error
+
+git checkout source
+jekyll build
+
+git branch -D master
+git checkout -b master
+git add _site/
+git commit -m "Added _site"
+git filter-branch --subdirectory-filter _site/ -f
+git filter-branch subdirectory-fi
