a81721ff86cda1c9950afc79db25f01f77016e3f
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2018-09-02 23:54:11 +0200

    .

--- _notebooks/nbc.py
+++ _notebooks/nbc.py
@@ -1,5 +1,7 @@
 c = get_config()
-c.NbConvertApp.notebooks = ['*.ipynb']
-c.Exporter.preprocessors = ['jekyll_preprocessor.JekyllPreprocessor']
+# c.NbConvertApp.notebooks = ['*.ipynb']
 c.NbConvertApp.export_format = 'markdown'
+c.NbConvertApp.output_files_dir='../images'
+c.Exporter.preprocessors = ['jekyll_preprocessor.JekyllPreprocessor']
 c.Exporter.template_file = 'jekyll'
+c.ExtractOutputPreprocessor.enabled = False
