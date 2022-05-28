3922619f1a8d1e2f975a14b29f34d0a61eb452fc
Heinrich Hartmann <derhein@gmail.com>
2022-05-28 23:25:34 +0000

    Estimates working

--- sampling/src/App.vue
+++ sampling/src/App.vue
@@ -1,46 +1,42 @@
 <script setup>
 import { ref, watchEffect } from 'vue'
 
-const srate = ref(50)
-const rps = ref(10)
-const epct = ref(3)
-const ttext = ref("1")
-const tunit = ref("min")
+const sampling_rate = ref(50)
+const request_rate = ref(10)
+const error_rate = ref(3)
+const time_window_text = ref("1")
+const time_window_unit = ref("min")
 
-const count = ref()
-const count_err = ref()
-const err_count = ref()
-const err_count_err = ref()
-const err_rate = ref()
-const err_rate_err = ref()
+const sim_iteration = ref(0)
+const sim_count = ref()
+const sim_count_err = ref()
+const sim_err_count = ref()
+const sim_err_count_err = ref()
+const sim_err_rate = ref()
+const sim_err_rate_err = ref()
 
-const ecount = ref()
+const est_count = ref()
+const est_count_err = ref()
+const est_err_count = ref()
+const est_err_count_err = ref()
+
 const twindow = ref()
-const eerr = ref(123)
 
-const epct_err = ref()
-
-const sim_count = ref(0)
-
-function sampling_error(r) {
-  var s = srate.value/100;
-  return Math.sqrt(r * (1 - s) / s);
+function sampling_error(c, p) {
+  return Math.sqrt(c * (1 - p) / p);
 }
-function calc(v) {
-  try { return Number(eval(ttext.value)); }
+function calc_window_text() {
+  try { return Number(eval(time_window_text.value)); }
   catch (err) { return 0; }
 }
 
-function B(n, k) {
-  if (Number.isNaN(n) || Number.isNaN(k)) return NaN;
-  if (k < 0 || k > n) return 0;
-  if (k === 0 || k === n) return 1;
-  if (k === 1 || k === n - 1) return n;
-  if (n - k < k) k = n - k;
-  let res = n;
-  for (let j = 2; j <= k; j++) res *= (n - j + 1) / j;
-  return res;
-};
+function do_estimate() {
+  const p = sampling_rate.value/100;
+  est_count.value = twindow.value * request_rate.value;
+  est_count_err.value = sampling_error(est_count.value, p);
+  est_err_count.value = est_count.value * error_rate.value / 100;
+  est_err_count_err.value = sampling_error(est_err_count.value, p);
+}
 
 function sample(X,p) {
   const out = [];
@@ -82,6 +78,7 @@ function stat_std(X) {
   return Math.sqrt(stat_var(X));
 }
 const stat_err = stat_sum; // error count
+
 function stat_err_rate(X) {
   let n = stat_cnt(X);
   if (n == 0) { return 0; }
@@ -102,9 +99,9 @@ var sim_generation = 0;
 function do_simulate() {
   sim_generation += 1;
   var my_generation = sim_generation;
-  const N = twindow.value * rps.value;
-  const K = N * epct.value / 100;
-  const p = srate.value/100;
+  const N = twindow.value * request_rate.value;
+  const K = N * error_rate.value / 100;
+  const p = sampling_rate.value/100;
   const X = new_set(N, K);
   // const D = simulate(X, stat_len, 50, 0.5);
   const X_CNT = [];
@@ -118,40 +115,36 @@ function do_simulate() {
     console.log("iter!");
     const S = sample(X, p);
     X_CNT.push(stat_cnt(S) / p);
-    X_ERR.push(stat_err(S));
+    X_ERR.push(stat_err(S) / p);
     X_ERR_RATE.push(stat_err_rate(S) * 100);
     if (cnt < cnt_max) setTimeout(iter,0);
   }
   function show() {
     if (sim_generation != my_generation) { return; }
     if (cnt < cnt_max) setTimeout(show, 100);
-    count.value = stat_mean(X_CNT);
-    count_err.value = stat_std(X_CNT);
-    err_count.value = stat_mean(X_ERR);
-    err_count_err.value = stat_std(X_ERR);
-    err_rate.value = stat_mean(X_ERR_RATE);
-    err_rate_err.value = stat_std(X_ERR_RATE);
-    sim_count.value = cnt;
+    sim_count.value = stat_mean(X_CNT);
+    sim_count_err.value = stat_std(X_CNT);
+    sim_err_count.value = stat_mean(X_ERR);
+    sim_err_count_err.value = stat_std(X_ERR);
+    sim_err_rate.value = stat_mean(X_ERR_RATE);
+    sim_err_rate_err.value = stat_std(X_ERR_RATE);
+    sim_iteration.value = cnt;
   }
   iter();
   show();
 }
 
 watchEffect(() => {
-  const u = tunit.value;
-  const v = calc(ttext.value);
-  const s = srate.value/100;
+  const u = time_window_unit.value;
+  const v = calc_window_text();
+  const s = sampling_rate.value/100;
   if (u == "sec")  { twindow.value = v }
   if (u == "min")  { twindow.value = v * 60 }
   if (u == "hour") { twindow.value = v * 60 * 60 }
   if (u == "day") { twindow.value = v * 60 * 60 * 24 }
 
+  do_estimate();
   do_simulate();
-  // count.value = twindow.value * rps.value;
-  // ecount.value = count.value * epct.value / 100;
-  // count_err.value = sampling_error(count.value) / count.value * 100;
-  // eerr.value = sampling_error(ecount.value)/count.value * 100;
-  // eerr.value = error_error(ecount.value);
 })
 </script>
 
@@ -171,33 +164,31 @@ Calculate effects of sampling to accuracy of request-rate and error-rate calcula
   <tbody>
     <tr>
       <td>Sampling Rate</td>
-      <td><input type="text" v-model="srate"> %</td>
-      <td><input type="range" min="0" max="100" v-model="srate" class="slider"></td>
+      <td><input type="text" v-model="sampling_rate"> %</td>
+      <td><input type="range" min="0" max="100" v-model="sampling_rate" class="slider"></td>
     </tr>
     <tr>
       <td>Request Rate</td>
-      <td><input type="text" v-model="rps"> rps</td>
-      <td><input type="range" min="0" max="1000" v-model="rps" class="slider"></td>
+      <td><input type="text" v-model="request_rate"> rps</td>
+      <td><input type="range" min="0" max="1000" v-model="request_rate" class="slider"></td>
     </tr>
     <tr>
       <td>Error Rate</td>
-      <td><input type="text" v-model="epct"> %</td>
-      <td><input type="range" min="0" max="100" v-model="epct" class="slider"></td>
+      <td><input type="text" v-model="error_rate"> %</td>
+      <td><input type="range" min="0" max="100" v-model="error_rate" class="slider"></td>
     </tr>
     <tr>
       <td>Time window</td>
       <td colspan="2">
-        <input type="text" v-model="ttext" style="margin-right:3px">
-        <select v-model="tunit"><option>sec</option><option>min</option><option>hour</option><option>day</option></select>
+        <input type="text" v-model="time_window_text" style="margin-right:3px">
+        <select v-model="time_window_unit"><option>sec</option><option>min</option><option>hour</option><option>day</option></select>
         ({{ twindow }} sec)
       </td>
     </tr>
 </tbody>
 </table>
 
-<h2>Simulation</h2>
-  <span style="float:right">{{ sim_count }} iterations</span>
-  <br><br>
+<h2>Estimation</h2>
   <table>
   <thead>
    <tr>
@@ -209,27 +200,59 @@ Calculate effects of sampling to accuracy of request-rate and error-rate calcula
   <tbody>
     <tr>
       <td>Request Count</td>
-      <td class="cell">{{ Number(count).toFixed(1) }} req</td>
-      <td class="cell"> ± {{ Number(count_err).toFixed(2) }} req</td>
-      <td class="cell"> {{ Number(count_err/count * 100).toFixed(2) }}%</td>
+      <td class="cell"> {{ Number(est_count).toFixed(1) }} req</td>
+      <td class="cell"> ± {{ Number(est_count_err).toFixed(2) }} req</td>
+      <td class="cell"> {{ Number(est_count_err/est_count * 100).toFixed(2) }}%</td>
     </tr>
     <tr>
       <td>Request Rate</td>
-      <td class="cell">{{ Number(count / twindow).toFixed(1) }} rps</td>
-      <td class="cell">± {{ Number(count_err / twindow).toFixed(2) }} rps </td>
-      <td class="cell">{{ (Number(count_err)/count * 100).toFixed(2) }}%</td>
+      <td class="cell">{{ Number(est_count / twindow).toFixed(1) }} rps</td>
+      <td class="cell">± {{ Number(est_count_err / twindow).toFixed(2) }} rps</td>
+      <td class="cell">{{ (Number(est_count_err)/est_count * 100).toFixed(2) }}%</td>
     </tr>
     <tr>
       <td>Error Count</td>
-      <td class="cell">{{ Number(err_count).toFixed(1) }} err</td>
-      <td class="cell">± {{ Number(err_count_err).toFixed(2) }} err</td>
-      <td class="cell">{{ Number(err_count_err / err_count * 100).toFixed(2) }}%</td>
+      <td class="cell">{{ Number(est_err_count).toFixed(1) }} err</td>
+      <td class="cell">± {{ Number(est_err_count_err).toFixed(2) }} err</td>
+      <td class="cell">{{ Number(est_err_count_err / est_err_count * 100).toFixed(2) }}%</td>
+    </tr>
+</tbody>
+</table>
+
+<h2>Simulation</h2>
+  <span style="float:right; margin-top: -50px;">{{ sim_iteration }} iterations</span>
+  <table>
+  <thead>
+   <tr>
+      <td width="220px">Statistic</td>
+      <td width="160px">Value</td>
+      <td style="widht:100%" colspan="2">Error</td>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Request Count</td>
+      <td class="cell">{{ Number(sim_count).toFixed(1) }} req</td>
+      <td class="cell"> ± {{ Number(sim_count_err).toFixed(2) }} req</td>
+      <td class="cell"> {{ Number(sim_count_err/sim_count * 100).toFixed(2) }}%</td>
+    </tr>
+    <tr>
+      <td>Request Rate</td>
+      <td class="cell">{{ Number(sim_count / twindow).toFixed(1) }} rps</td>
+      <td class="cell">± {{ Number(sim_count_err / twindow).toFixed(2) }} rps</td>
+      <td class="cell">{{ (Number(sim_count_err)/sim_count * 100).toFixed(2) }}%</td>
+    </tr>
+    <tr>
+      <td>Error Count</td>
+      <td class="cell">{{ Number(sim_err_count).toFixed(1) }} err</td>
+      <td class="cell">± {{ Number(sim_err_count_err).toFixed(2) }} err</td>
+      <td class="cell">{{ Number(sim_err_count_err / sim_err_count * 100).toFixed(2) }}%</td>
     </tr>
     <tr>
       <td>Error Rate</td>
-      <td class="cell">{{ Number(err_rate).toFixed(2) }} %</td>
-      <td class="cell">± {{ Number(err_rate_err).toFixed(2) }} ppt</td>
-      <td class="cell">{{ Number(err_rate_err / err_rate * 100).toFixed(2) }}%</td>
+      <td class="cell">{{ Number(sim_err_rate).toFixed(2) }} %</td>
+      <td class="cell">± {{ Number(sim_err_rate_err).toFixed(2) }} ppt</td>
+      <td class="cell">{{ Number(sim_err_rate_err / sim_err_rate * 100).toFixed(2) }}%</td>
     </tr>
 </tbody>
 </table>
